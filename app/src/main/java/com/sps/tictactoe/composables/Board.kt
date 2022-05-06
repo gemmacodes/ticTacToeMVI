7865f6d6ba0811cdc40b5f9c92b1f686dbb00be2
switcherette <gfornons@gmail.com>
2022-05-06 11:45:00 +0200

    animated pieces wip

--- app/src/main/java/com/sps/tictactoe/composables/Board.kt
+++ app/src/main/java/com/sps/tictactoe/composables/Board.kt
@@ -2,6 +2,9 @@ package com.sps.tictactoe.composables
 
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
@@ -82,23 +85,41 @@ fun PieceO(color: Color = Pine) {
             .fillMaxWidth(),
         contentAlignment = Alignment.Center,
     ) {
+        val animateFloat = remember { Animatable(0f) }
+        LaunchedEffect(animateFloat) {
+            animateFloat.animateTo(
+                targetValue = 1f,
+                animationSpec = tween(durationMillis = 500, easing = LinearEasing))
+        }
         Canvas(modifier = Modifier.fillMaxSize()) {
-            val canvasWidth = size.width
-            val canvasHeight = size.height
+//            val canvasWidth = size.width
+//            val canvasHeight = size.height
             val radius = (size.minDimension / 2) * 0.70f
             val stroke = radius * 0.1f.dp.toPx()
-            drawCircle(
+            val sizeArc = size/1.75F
+
+            drawArc(
                 color = color,
-                center = Offset(x = canvasWidth / 2, y = canvasHeight / 2),
-                radius = radius,
+                startAngle = 0f,
+                sweepAngle = 360f * animateFloat.value,
+                useCenter = false,
+                topLeft = Offset((size.width - sizeArc.width)/2f,(size.height - sizeArc.height)/2f),
+                size = sizeArc,
                 style = Stroke(width = stroke)
             )
-            drawCircle(
-                color = color,
-                center = Offset(x = canvasWidth / 2, y = canvasHeight / 2),
-                radius = (size.minDimension / 2) * 0.35f,
-                style = Stroke(width = stroke)
-            )
+
+//            drawCircle(
+//                color = color,
+//                center = Offset(x = canvasWidth / 2, y = canvasHeight / 2),
+//                radius = radius,
+//                style = Stroke(width = stroke)
+//            )
+//            drawCircle(
+//                color = color,
+//                center = Offset(x = canvasWidth / 2, y = canvasHeight / 2),
+//                radius = (size.minDimension / 2) * 0.35f,
+//                style = Stroke(width = stroke)
+//            )
         }
     }
 }
@@ -110,49 +131,90 @@ fun PieceX(color: Color = Mauve) {
             .fillMaxWidth(),
         contentAlignment = Alignment.Center,
     ) {
+        val animateFloat = remember { Animatable(0f) }
+        LaunchedEffect(animateFloat) {
+            animateFloat.animateTo(
+                targetValue = 1f,
+                animationSpec = tween(durationMillis = 250, easing = LinearEasing))
+        }
+
+        val animateFloat2 = remember { Animatable(1f) }
+        LaunchedEffect(animateFloat2) {
+            animateFloat.animateTo(
+                targetValue = 0f,
+                animationSpec = tween(durationMillis = 250, delayMillis = 250, easing = LinearEasing))
+        }
+
         Canvas(modifier = Modifier.fillMaxSize()) {
             val canvasWidth = size.width
             val canvasHeight = size.height
             val rectH = canvasHeight * 0.75f
-            val rectW = rectH * 0.25f
-            drawRect(
-                color = color,
-                topLeft = Offset(
-                    x = (canvasWidth / 2F) - rectW / 2f,
-                    y = (canvasHeight - rectH) / 2f
+            val rectW = rectH * 0.15f
+
+            drawLine(
+                start = Offset(
+                    x = (3 * canvasWidth) / 4F,
+                    y =  canvasHeight / 4F
                 ),
-                size = Size(height = rectH, width = rectW)
+                end = Offset(
+                    x = (canvasWidth/4F) * animateFloat.value,
+                    y = ((3 * canvasHeight) / 4F) * animateFloat.value),
+                color = color,
+                strokeWidth = rectW
             )
-            rotate(45f) {
-                drawRect(
-                    color = color,
-                    topLeft = Offset(
-                        x = (canvasWidth / 2F) - rectW / 2f,
-                        y = (canvasHeight - rectH) / 2f
-                    ),
-                    size = Size(height = rectH, width = rectW)
-                )
-            }
-            rotate(90f) {
-                drawRect(
-                    color = color,
-                    topLeft = Offset(
-                        x = (canvasWidth / 2F) - rectW / 2f,
-                        y = (canvasHeight - rectH) / 2f
-                    ),
-                    size = Size(height = rectH, width = rectW)
-                )
-            }
-            rotate(135f) {
-                drawRect(
-                    color = color,
-                    topLeft = Offset(
-                        x = (canvasWidth / 2F) - rectW / 2f,
-                        y = (canvasHeight - rectH) / 2f
-                    ),
-                    size = Size(height = rectH, width = rectW)
-                )
-            }
+
+            drawLine(
+                start = Offset(
+                    x= canvasWidth / 4F,
+                    y = canvasHeight / 4F
+                ),
+                end = Offset(
+                    x = ((3 * canvasWidth) / 4f) * animateFloat.value,
+                    y = ((3 * canvasHeight) / 4f) * animateFloat.value
+                ),
+                color = Mauve,
+                strokeWidth = rectW
+            )
+
+
+//            drawRect(
+//                color = color,
+//                topLeft = Offset(
+//                    x = (canvasWidth / 2F) - rectW / 2f,
+//                    y = (canvasHeight - rectH) / 2f
+//                ),
+//                size = Size(height = rectH, width = rectW)
+//            )
+//            rotate(45f) {
+//                drawRect(
+//                    color = color,
+//                    topLeft = Offset(
+//                        x = (canvasWidth / 2F) - rectW / 2f,
+//                        y = (canvasHeight - rectH) / 2f
+//                    ),
+//                    size = Size(height = rectH, width = rectW)
+//                )
+//            }
+//            rotate(90f) {
+//                drawRect(
+//                    color = color,
+//                    topLeft = Offset(
+//                        x = (canvasWidth / 2F) - rectW / 2f,
+//                        y = (canvasHeight - rectH) / 2f
+//                    ),
+//                    size = Size(height = rectH, width = rectW)
+//                )
+//            }
+//            rotate(135f) {
+//                drawRect(
+//                    color = color,
+//                    topLeft = Offset(
+//                        x = (canvasWidth / 2F) - rectW / 2f,
+//                        y = (canvasHeight - rectH) / 2f
+//                    ),
+//                    size = Size(height = rectH, width = rectW)
+//                )
+//            }
         }
     }
 }
